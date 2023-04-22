0a9a9bec487450df91975e6d85eec985597f6a94
Haryish Elangumaran <60095814+Haryish@users.noreply.github.com>
2023-04-22 19:50:01 +0530

    Modified COde

--- Coprime.py
+++ Coprime.py
@@ -2,22 +2,27 @@
 """
 Created on Sat Mar 25 13:13:43 2023
 
-@author: haryi
+@author: Haryish Elangumaran
 """
 
-def coprime(x,y):
-    if len(set(factors(x)) & set(factors(y))) == 0:
-        print('{} and {} are coprime'.format(x,y))
-    else:
-        print('{} and {} are not coprime'.format(x,y))
+
+def coprimenumber(a, b):
+  x = set(factors(a))
+  y = set(factors(b))
+  hcf = set(x.intersection(y))
+  print(a, b, hcf)
+  if hcf == {1}:
+    print('{} and {} are coprime'.format(a, b))
+  else:
+    print('{} and {} are not coprime'.format(a, b))
+
+
 def factors(i):
-    fac = []
-    for j in range(2,i+1):
-        if i%j == 0:
-            fac.append(j)
-    return fac
+  fac = [1]
+  for j in range(2, i + 1):
+    if i % j == 0:
+      fac.append(j)
+  return fac
 
-
-#eg
-coprime(17,15)
-coprime(10,8)
+print("As Per code,")
+coprimenumber(14,15)
