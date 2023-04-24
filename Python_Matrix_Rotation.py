4a4586f9b672c2f08c9f1fdc5a336230b283140a
Haryish <haryishkumaran16@gmail.com>
2023-04-24 19:36:23 +0530

    Modified A2Q2

--- Python_Matrix_Rotation.py
+++ Python_Matrix_Rotation.py
@@ -5,61 +5,70 @@ Created on Sat Mar 25 13:26:44 2023
 @author: Haryish Elangumaran
 """
 
-def rotate_matrix(matrix, degrees):
-    """
-    Rotates a given matrix by the specified number of degrees.
+def rotate_matrix(matrix, rotation):
+    """Function to rotate the matrix by 90, 180, 270, or 360 degrees."""
+    rows = len(matrix)
+    cols = len(matrix[0])
+    rotated_matrix = []
 
-    Args:
-        matrix (list): The matrix to rotate.
-        degrees (int): The number of degrees to rotate the matrix (must be a multiple of 90).
+    #rotates matrix to 90 degree
+    if rotation == 90:
+        for j in range(cols):
+            row = []
+            for i in range(rows - 1, -1, -1):
+                row.append(matrix[i][j])
+            rotated_matrix.append(row)
 
-    Returns:
-        The rotated matrix.
-    """
-    if degrees % 90 != 0:
-        raise ValueError("Degrees must be a multiple of 90.")
+    #rotates matrix to 180 degree
+    elif rotation == 180:
+        for i in range(rows - 1, -1, -1):
+            rotated_matrix.append(matrix[i][::-1])
 
-    # Calculate the number of 90-degree rotations to perform.
-    num_rotations = degrees // 90
+    #rotates matrix to 270 degree
+    elif rotation == 270:
+        for j in range(cols - 1, -1, -1):
+            row = []
+            for i in range(rows):
+                row.append(matrix[i][j])
+            rotated_matrix.append(row)
 
-    # Perform the rotations.
-    for i in range(num_rotations):
-        # Transpose the matrix (switch rows and columns).
-        matrix = list(zip(*matrix))
+    #Rotates batrix to 360 degree        
+    elif rotation == 360:
+        rotated_matrix = matrix
 
-        # Reverse each row to rotate 90 degrees clockwise or 270 degrees counter-clockwise.
-        if degrees % 360 != 0:
-            matrix = [list(reversed(row)) for row in matrix]
+    return rotated_matrix
 
-    return matrix
-
-
-# Example matrix.
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-# Menu loop.
-while True:
-    print("Current matrix:")
+def print_matrix(matrix):
+    """Function to print the matrix in a user-friendly way."""
     for row in matrix:
         print(row)
 
-    print("1. Rotate 90 degrees clockwise")
-    print("2. Rotate 180 degrees")
-    print("3. Rotate 270 degrees counter-clockwise")
-    print("4. Rotate 360 degrees")
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+while True:
+    print("Matrix Rotation Menu")
+    print("1. Rotate by 90 degrees")
+    print("2. Rotate by 180 degrees")
+    print("3. Rotate by 270 degrees")
+    print("4. Rotate by 360 degrees")
     print("5. Exit")
-
-    choice = int(input("Enter your choice (1-5): "))
-
+    choice = int(input("Enter your choice: "))
+    
     if choice == 1:
         matrix = rotate_matrix(matrix, 90)
+        print_matrix(matrix)
     elif choice == 2:
         matrix = rotate_matrix(matrix, 180)
+        print_matrix(matrix)
     elif choice == 3:
         matrix = rotate_matrix(matrix, 270)
+        print_matrix(matrix)
     elif choice == 4:
         matrix = rotate_matrix(matrix, 360)
+        print_matrix(matrix)
     elif choice == 5:
         break
     else:
-        print("Invalid choice.")
+        print("Invalid choice. Please try again.")
